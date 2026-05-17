797fe7f825dc7fcdf1f08d3dec254cf349d6c85f
Mateo Emanuel Urquiza <mateoema1@gmail.com>
2026-05-17 03:20:59 -0300

    Validaciones del EJ2

--- TP5_Grupo7_PIII/ListarSucursal.aspx.cs
+++ TP5_Grupo7_PIII/ListarSucursal.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Configuration;
@@ -30,17 +31,20 @@ namespace TP5_Grupo7_PIII
             ddlProvincias.DataValueField = "Id_Provincia";
             ddlProvincias.DataBind();
         }
+
         private void CargarSucursales()
         {
             gvListar.DataSource = sucursales.obtenerSucursales();
             gvListar.DataBind();
         }
+
         protected void butMostrarT_Click(object sender, EventArgs e)
         {
             Limpiarcampos();
+            // Se oculta el mensaje de error al mostrar todos
+            lblMensaje.Visible = false;
             CargarSucursales();
         }
-
 
         protected void butFiltrar_Click(object sender, EventArgs e)
         {
@@ -51,7 +55,38 @@ namespace TP5_Grupo7_PIII
                 lblMensaje.Visible = true;
                 return;
             }
-            gvListar.DataSource = sucursales.filtrarSucursal(idSucursal);
+
+            // Validacion que el ID sea un numero
+            foreach (char c in tbIdSuc.Text.Trim())
+            {
+                if (!char.IsDigit(c))
+                {
+                    lblMensaje.Text = "El ID de sucursal debe ser un número.";
+                    lblMensaje.Visible = true;
+                    return;
+                }
+            }
+
+            // Se guarda el resultado para verificar si hay datos
+            DataTable resultado = sucursales.filtrarSucursal(idSucursal);
+            bool hayDatos = false;
+            foreach (DataRow fila in resultado.Rows)
+            {
+                hayDatos = true;
+                break;
+            }
+
+            if (!hayDatos)
+            {
+                lblMensaje.Text = "No se encontraron sucursales con ese ID.";
+                lblMensaje.Visible = true;
+            }
+            else
+            {
+                lblMensaje.Visible = false;
+            }
+
+            gvListar.DataSource = resultado;
             gvListar.DataBind();
             Limpiarcampos();
         }
@@ -79,11 +114,29 @@ namespace TP5_Grupo7_PIII
         protected void butFiltrarprov_Click(object sender, EventArgs e)
         {
             string idProvincia = ddlProvincias.SelectedValue;
-            gvListar.DataSource = sucursales.filtrarSucursalProv(Convert.ToInt32(idProvincia));
+
+            // Se verifica si hay datos al filtrar por provincia
+            DataTable resultado = sucursales.filtrarSucursalProv(Convert.ToInt32(idProvincia));
+            bool hayDatos = false;
+            foreach (DataRow fila in resultado.Rows)
+            {
+                hayDatos = true;
+                break;
+            }
+
+            if (!hayDatos)
+            {
+                lblMensaje.Text = "No se encontraron sucursales para esa provincia.";
+                lblMensaje.Visible = true;
+            }
+            else
+            {
+                lblMensaje.Visible = false;
+            }
+
+            gvListar.DataSource = resultado;
             gvListar.DataBind();
             Limpiarcampos();
-
-
         }
     }
 }
